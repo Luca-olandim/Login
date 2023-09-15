4b0faafe233cd54dc127bd676c05e6d3be527e18
Luca-olandim <127211138+Luca-olandim@users.noreply.github.com>
2023-09-15 12:47:04 -0300

    Add files via upload

---
+++ Página Login/script.js
@@ -0,0 +1,32 @@
+const campoLogin = document.getElementById("login")
+const campoSenha = document.getElementById("password")
+const campoNovoLogin = document.getElementById("novoLogin")
+const campoNovaSenha = document.getElementById("novaSenha")
+const campoRepSenha = document.getElementById("repSenha")
+
+let usuarios = [];
+
+function login(){
+    let mensagem = "Usuário ou senha incorreta!";
+    for(let usuario of usuarios){
+        if(usuario.login == campoLogin.value && usuario.senha == campoSenha.value){
+            mensagem = "Logado com sucesso!"
+            break;
+        }
+    }
+    alert(mensagem)
+}
+
+function cadastra(){
+if(campoNovaSenha.value = campoRepSenha.value){
+    let usuario = {
+        login : campoNovoLogin.value,
+        senha : campoNovaSenha.value
+    }
+    usuarios.push(usuario);
+    alert("Usuáiro cadastrado com sucesso!")
+}
+else{
+    alert("Você digitou duas senhas diferentes!")
+}
+}
